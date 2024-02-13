261d318c6ff3a10b388ff3fade908eaa9de9f65e
Jenifer <jeni@Daniels-MacBook-Pro.local>
2024-02-13 17:14:03 -0500

    Initial Commit

---
+++ sampleMavenProject/src/main/java/com/mycomp/petproject/sampleMavenProject/App.java
@@ -0,0 +1,13 @@
+package com.mycomp.petproject.sampleMavenProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
